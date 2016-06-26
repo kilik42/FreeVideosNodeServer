21219582816a2da977a7dc2dc1255cf15f3234e6
marvin evins <maevins@cps.edu>
2016-06-25 20:55:34 -0500

    NSURLSession, Blocks and talking to node server

--- FreeVideosApp/HTTPService.m
+++ FreeVideosApp/HTTPService.m
@@ -7,6 +7,8 @@
 //
 
 #import "HTTPService.h"
+#define URL_BASE "http://localhost:8000"
+#define URL_TUTORIALS "/tutorials"
 
 @implementation HTTPService
 
@@ -19,6 +21,30 @@
     }
     
     return sharedInstance;
+}
+
+-(void) getTutorials{
+    
+    NSURL *url = [NSURL URLWithString: [NSString stringWithFormat:@"%s%s", URL_BASE, URL_TUTORIALS]];
+    NSURLSession *session = [NSURLSession sharedSession];
+    
+    [[session dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        if(data != nil){
+            NSError *err;
+            NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&err ];
+            
+            if (err == nil){
+                NSLog(@"JSON: %@", json.debugDescription);
+            }
+            
+            else {
+                NSLog(@"Err: %@", error.debugDescription);
+            }
+        }
+        
+        
+    }] resume];
 }
 
 @end
